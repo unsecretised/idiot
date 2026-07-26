53b30045f4951a8c4bba5c40503b3c8423f4238a
unsecretised <no1umang@gmail.com>
2026-07-26 14:57:39 +0800

    Initial Commit

---
+++ idiot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  idiot
+//
+//  Created by Umang on 26/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ idiot/idiotApp.swift
@@ -0,0 +1,17 @@
+//
+//  idiotApp.swift
+//  idiot
+//
+//  Created by Umang on 26/7/26.
+//
+
+import SwiftUI
+
+@main
+struct idiotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
